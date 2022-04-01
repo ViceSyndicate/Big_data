ded71bc104d8fe026bea9e351f6d7e1e269b58cd
Victor S <victor.skarerverket@gmail.com>
2022-04-02 01:44:02 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+def extract_data():
+    unclean = open('male.csv', 'r')
+
+
+    cleaned = open(f'male_cleaned_data.json', 'w')
+
+    lines = unclean.readlines()
+    for line in lines:
+        cleaned.writelines()
+
+
+
+if __name__ == '__main__':
+    extract_data()
